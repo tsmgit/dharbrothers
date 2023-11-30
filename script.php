b7f29e35d1a4180f33855b15605ebcf4f0d7f1bb
unknown <it@thinksurfmedia.com>
2023-11-30 10:51:24 +0530

    10th

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+//user:cinecafes
+//Password:derdWKqa0782Z
+//https://github.com/cinecafes/cinecafes.git
+
+exec("git pull https://github.com/tsmgit/dharbrothers.git main");
+?>
+exec("cd /home6/thinksurfmedia/dharbrothers.thinksurfmedia.in/php/");
+exec("composer install");
+exec("source /home6/thinksurfmedia/nodevenv/dharbrothers.thinksurfmedia.in/php/10/bin/activate && cd /home6/thinksurfmedia/dharbrothers.thinksurfmedia.in/php");
+exec("npm install");
