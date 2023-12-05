6547552122fd44bf06a090ec8fd61f76ecb4afa2
unknown <it@thinksurfmedia.com>
2023-12-05 14:01:05 +0530

    11st commit

---
+++ composer-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change directory
+cd /home6/thinksurfmedia/dharbrothers.thinksurfmedia.in/php/
+
+composer install
+
+source /home6/thinksurfmedia/nodevenv/dharbrothers.thinksurfmedia.in/php/10/bin/activate && cd /home6/thinksurfmedia/dharbrothers.thinksurfmedia.in/php");
+npm install
